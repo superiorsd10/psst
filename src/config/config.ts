aa03e08e3106bf4b8f931ad60aede16f56028639
superiorsd10 <sachindapkara6@gmail.com>
2024-08-05 01:42:41 +0530

    chore(config): add jwt secret

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,8 @@ dotenv.config();
 
 const config = {
     port: process.env.PORT || 3000,
-    redisUrl: process.env.REDIS_URL || 'redis://localhost:6379'
+    redisUrl: process.env.REDIS_URL || 'redis://localhost:6379',
+    jwtSecret: process.env.JWT_SECRET || ''
 };
 
 export default config;
