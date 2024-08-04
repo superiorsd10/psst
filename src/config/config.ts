5634dc961a93ef2296860ff8975635e20a003704
superiorsd10 <sachindapkara6@gmail.com>
2024-08-05 01:03:55 +0530

    chore(config): add redis url

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    redisUrl: process.env.REDIS_URL || 'redis://localhost:6379'
 };
 
 export default config;
