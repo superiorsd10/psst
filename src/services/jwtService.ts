caab884bfe6c828d1a166c19742f56c75be1a6c1
superiorsd10 <sachindapkara6@gmail.com>
2024-08-05 01:47:51 +0530

    feat(services): add jwt service

---
+++ src/services/jwtService.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { ApiError } from '../utils/apiError';
+import config from '../config/config';
+
+class JwtService {
+    private readonly secret: string;
+    private readonly expiresIn: string;
+
+    constructor() {
+        (this.secret = config.jwtSecret), (this.expiresIn = '1d');
+    }
+
+    generateToken(payload: object): string {
+        return jwt.sign(payload, this.secret, { expiresIn: this.expiresIn });
+    }
+
+    verifyToken(token: string): any {
+        try {
+            return jwt.verify(token, this.secret);
+        } catch (error) {
+            throw ApiError.unauthorized('Invalid token');
+        }
+    }
+}
+
+export const jwtService = new JwtService();
