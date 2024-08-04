4dba29550f0a11aecb8bbd9580ad5f363a646dda
superiorsd10 <sachindapkara6@gmail.com>
2024-08-04 23:27:30 +0530

    feat(utils): add api error class

---
+++ src/utils/apiError.ts
@@ -0,0 +1,28 @@
+import { HttpStatusCode } from './httpStatusCodes';
+
+export class ApiError extends Error {
+    constructor(
+        public code: number,
+        message: string
+    ) {
+        super(message);
+        this.name = 'ApiError';
+        Object.setPrototypeOf(this, new.target.prototype);
+    }
+
+    static badRequest(msg: string): ApiError {
+        return new ApiError(HttpStatusCode.BAD_REQUEST, msg);
+    }
+
+    static unauthorized(msg: string): ApiError {
+        return new ApiError(HttpStatusCode.UNAUTHORIZED, msg);
+    }
+
+    static notFound(msg: string): ApiError {
+        return new ApiError(HttpStatusCode.NOT_FOUND, msg);
+    }
+
+    static internal(msg: string): ApiError {
+        return new ApiError(HttpStatusCode.INTERNAL_SERVER_ERROR, msg);
+    }
+}
