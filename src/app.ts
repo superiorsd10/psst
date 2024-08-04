08313c3c1c538ddb427533917ea99a5c9d57dd86
superiorsd10 <sachindapkara6@gmail.com>
2024-08-05 01:15:33 +0530

    chore(src): add rate limiter and error handler middlewares to app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Express } from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
+import { rateLimiterMiddleware } from './api/middlewares/rateLimiterMiddleware';
+import { errorHandlerMiddleware } from './api/middlewares/errorHandlerMiddleware';
 
 const createApp = (): Express => {
     const app = express();
@@ -10,6 +12,10 @@ const createApp = (): Express => {
     app.use(morgan('dev'));
     app.use(cors());
     app.use(express.json());
+
+    app.use(rateLimiterMiddleware);
+
+    app.use(errorHandlerMiddleware);
 
     return app;
 };
