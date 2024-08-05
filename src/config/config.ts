2dd236448efe115e87a3128bfacd5c07bc979fc6
superiorsd10 <sachindapkara6@gmail.com>
2024-08-05 16:24:27 +0530

    chore(config): add aws config

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,11 @@ dotenv.config();
 const config = {
     port: process.env.PORT || 3000,
     redisUrl: process.env.REDIS_URL || 'redis://localhost:6379',
-    jwtSecret: process.env.JWT_SECRET || ''
+    jwtSecret: process.env.JWT_SECRET || '', 
+    awsRegion: process.env.AWS_REGION || '',
+    awsS3Bucket: process.env.AWS_S3_BUCKET || '',
+    awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
 };
 
 export default config;
