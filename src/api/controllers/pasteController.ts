d0474daf617cc0c665143104bb2813ca93a5676e
superiorsd10 <sachindapkara6@gmail.com>
2024-08-05 17:45:45 +0530

    feat(controllers): add paste controller

---
+++ src/api/controllers/pasteController.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import { pasteService } from '../../services/pasteService';
+import { CreatePasteInput } from '../../schemas/pasteSchemas';
+
+class PasteController {
+    async createPaste(req: Request, res: Response, next: NextFunction) {
+        try {
+            const input: CreatePasteInput = req.body;
+            const userId = req.userId!;
+            const result = await pasteService.createPaste(input, userId);
+            res.status(201).json(result);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async getPaste(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { id } = req.params;
+            const { password } = req.query;
+            const paste = await pasteService.getPaste(
+                id,
+                password as string | undefined
+            );
+            res.json(paste);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+export const pasteController = new PasteController();
