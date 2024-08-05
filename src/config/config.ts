f95ef3b86f49f4a2da3401cf76ece1ea6163a34c
superiorsd10 <sachindapkara6@gmail.com>
2024-08-06 04:46:23 +0530

    docs(config): add jsdocs comments

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+/**
+ * Configuration object containing environment variables.
+ * @type {Object}
+ * @property {number|string} port - The port the server will listen on.
+ * @property {string} redisUrl - The URL for the Redis server.
+ * @property {string} jwtSecret - The secret key for JWT.
+ * @property {string} awsRegion - The AWS region.
+ * @property {string} awsS3Bucket - The name of the AWS S3 bucket.
+ * @property {string} awsAccessKeyId - The AWS access key ID.
+ * @property {string} awsSecretAccessKey - The AWS secret access key.
+ * @property {string} encryptionKey - The encryption key for securing content.
+ */
 const config = {
     port: process.env.PORT || 3000,
     redisUrl: process.env.REDIS_URL || 'redis://localhost:6379',
